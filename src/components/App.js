4072cfd2355e1ff32eecebd374693c1365c38004
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-09-03 19:48:25 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+
+  const[text, setText] = useState()
+
+  // useEffect(() => {
+  //   const getData = async() => {
+  //     const data = await fetch("https://dummyjson.com/products")
+  //     const json = await data.json()
+  //     setText(json)
+  //   }
+  //   getData()
+  // }, [])
+
+  useEffect(() => {
+    const getData = () => {
+      fetch("https://dummyjson.com/products")
+        .then((data) => data.json())
+        .then((json) => {
+          setText(json);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    };
+
+    getData();
+  }, []);
+
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <pre>
+         {JSON.stringify(text, null, 2)}
+        </pre>
     </div>
   )
 }
